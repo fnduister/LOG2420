b6f94a1d8dfb458e3e9d823149d4f228e0646b47
Fabrice Ndui <fnduister@gmail.com>
2019-02-15 13:58:24 -0500

    fixed form

--- TD2/factsage.js
+++ TD2/factsage.js
@@ -5,14 +5,14 @@ const handleSubmit = async formElement => {
 
   const init = {
     method: "POST",
-    body: data,
+    body: data
   };
 
   try {
     const dataElement = document.querySelector("#dataTable");
     dataElement.innerHTML = '<img src="img/loader.gif" alt="loader">';
     const response = await fetch("http://localhost:8080", init);
-    JSONResponse = await response.json();  
+    JSONResponse = await response.json();
     displayTable(JSONResponse, dataElement);
   } catch (err) {
     console.log(err);
@@ -20,5 +20,23 @@ const handleSubmit = async formElement => {
 };
 
 const displayTable = (data, parentElement) => {
-  
-}
+  const parametres = data.output1;
+  console.log({ parametres });
+  // <table id="param">
+  //     <tr>
+  //       <td>T</td>
+  //       <td>=</td>
+  //       <td>298.15 K</td>
+  //     </tr>
+  //     <tr>
+  //       <td>P</td>
+  //       <td>=</td>
+  //       <td>1 atm</td>
+  //     </tr>
+  //     <tr>
+  //       <td>V</td>
+  //       <td>=</td>
+  //       <td>0 dm3</td>
+  //     </tr>
+  //   </table>
+};
